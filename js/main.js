61049073ad3434c6c809df2520be5f60b6f726b8
Mohmmad Amir <76723565+Mohmmad-amir@users.noreply.github.com>
2023-02-16 23:00:04 +0600

    testing js data type (string)

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/*
+* printing hello world
+console.log('hello world');
+let string = typeof "amir"
+let number = typeof 47
+let bool = typeof true
+console.log(string);
+console.log(number);
+console.log(bool);
+*/
+
+// data type
+// string
+const math = "Mathematics"
+// the length property
+// console.log("every good boy does fine".length);
+// sting method
+console.log(math.charAt(11));
