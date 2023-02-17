cc81621b402c0e26e2d282f7aaa80c4a98c1a520
Mohmmad Amir <76723565+Mohmmad-amir@users.noreply.github.com>
2023-02-17 10:28:12 +0600

    if statement complete

--- js/ifStatement.js
+++ js/ifStatement.js
@@ -1,7 +1,61 @@
+let customerIsBanned = false;
 let soup = "Chicken noodle soup"
+let crackers = true;
 let reply;
-if (soup) {
+if (customerIsBanned) {
+    reply = "NO soup for you!"
+}
+else if (soup && crackers) {
+    // run code
+    reply = `here is your order of ${soup} & crackers`
+}
+else if (soup) {
     // run code
     reply = `here is your order of ${soup}`
+} else {
+    reply = `sorry we're out of ${soup}`
 }
-console.log(reply);
+// console.log(reply);
+
+let testSCore = 60;
+let collageStudent = true
+let grade;
+if (testSCore >= 90) {
+    grade = "A"
+} else if (testSCore >= 80) {
+    grade = "B"
+} else if (testSCore >= 70) {
+    grade = "C"
+} else if (testSCore >= 60) {
+    grade = "D"
+} else {
+    if (collageStudent) {
+        grade = "Unsatisfactory"
+    } else {
+        grade = "fail"
+    }
+}
+// console.log(grade);
+
+
+if (playerOne === computer) {
+    //tie game
+} else if (playerOne === "rock") {
+    if (computer === "paper") {
+        //computer wins
+    } else {
+        // playerOne wins
+    }
+} else if (playerOne === "paper") {
+    if (computer === "scissors") {
+        //computer wins
+    } else {
+        //playerOne wins
+    }
+} else {
+    if (computer === "rock") {
+        //computer wind
+    } else {
+        //playerOne wins
+    }
+}
