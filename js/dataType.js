32b951dd282f7e91c9a5f0a3199612d68de49bb1
Mohmmad Amir <76723565+Mohmmad-amir@users.noreply.github.com>
2023-02-17 09:46:30 +0600

    complete string and number data type & start math function

---
+++ js/dataType.js
@@ -0,0 +1,58 @@
+/*
+* printing hello world
+console.log('hello world');
+let string = typeof "amir"
+let number = typeof 47
+let bool = typeof true
+console.log(string);
+console.log(number);
+console.log(bool);
+*/
+
+// ? Data Type
+// ! Start string
+const math = "Mathematics"
+// !the length property
+// console.log("every good boy does fine".length);
+// !sting method
+// *chartAt() print the index number of a string
+// console.log(math.charAt(0));
+// *indexof() print the index number from start
+// console.log(math.indexOf("ati"));
+// console.log(math.indexOf("ath"));
+// *slice() print the between charrecter of the given values
+// console.log(math.slice(5, 6));
+// *toUpperCase print all string value to UpperCase
+// console.log(math.toUpperCase());
+// *toLowerCase print all string value to lowerCase
+// console.log(math.toLowerCase());
+// *includes print true/false
+// console.log(math.includes("mat"));
+// split() return array of the given string value
+// console.log(math.split('e'));
+// !End String
+
+
+// ! Start number type
+const number = 42
+const float = 42.045454587
+const string = "42.012sdfasdf"
+// console.log(string);
+// console.log(number === float);
+// console.log(string + 3);
+// *number() change data type to number.
+//* number () also return true/false as number like (1/0)
+// console.log(Number(string) === number);
+// console.log(Number(false));
+/*
+*the number.isInteger() method determines whether
+* the passed value is integer */
+// console.log(Number.isInteger(float));
+// console.log(Number.parseFloat(string));
+// console.log(float.toFixed(2));
+// console.log(Number.parseInt(string));
+// console.log(typeof float.toString());
+// console.log(typeof Number.parseFloat(string));
+// console.log(Number.parseFloat("4.12454asfjdh").toFixed(2).toString());
+
+// ! End Number Type
---
+++ js/math.js
@@ -0,0 +1,12 @@
+// *PI
+console.log(Math.PI);
+// *trunc
+console.log(Math.trunc(Math.PI));
+// *round
+console.log(Math.round(Math.PI));
+console.log(Math.round(5.258));
+//* ceil
+console.log(Math.ceil(7.2));
+console.log(Math.ceil(Math.PI));
+// *floor
+console.log(Math.floor(Math.PI));
